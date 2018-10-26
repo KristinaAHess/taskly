e059e15c48b0a0fe3c4db926b3d20bf7f8139cfc
Kristina Heß <kristina.hess@lht.dlh.de>
2018-10-26 11:56:35 +0100

    add Components for member module

--- src/app/member/member.module.ts
+++ src/app/member/member.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MemberListComponent } from './member-list/member-list.component';
+import { MemberDetailsComponent } from './member-details/member-details.component';
+import { MemberEditComponent } from './member-edit/member-edit.component';
+import { MemberCreateComponent } from './member-create/member-create.component';
+import { MemberDashboardComponent } from './member-dashboard/member-dashboard.component';
+import { MemberFormTemplateComponent } from './member-form-template/member-form-template.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [
+    MemberListComponent,
+    MemberDetailsComponent,
+    MemberEditComponent,
+    MemberCreateComponent,
+    MemberDashboardComponent,
+    MemberFormTemplateComponent]
 })
-export class MemberModule { }
+export class MemberModule {
+}
