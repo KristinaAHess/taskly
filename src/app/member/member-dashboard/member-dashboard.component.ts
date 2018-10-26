84d7b842ac106fd910988763e296f349e5720c39
Kristina Heß <kristina.hess@lht.dlh.de>
2018-10-26 18:24:04 +0100

    use ngrx for getting members

--- src/app/member/member-dashboard/member-dashboard.component.ts
+++ src/app/member/member-dashboard/member-dashboard.component.ts
@@ -1,11 +1,4 @@
-import { LoadMembersAction } from './../../state/member/member.actions';
 import { Component, OnInit } from '@angular/core';
-import { select, Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
-import { ApplicationState } from 'src/app/state/app.state';
-
-import { Member } from '../models/member';
-import { MembersQuery } from './../../state/member/member.reducer';
 
 @Component({
   selector: 'app-member-dashboard',
@@ -13,14 +6,10 @@ import { MembersQuery } from './../../state/member/member.reducer';
   styleUrls: ['./member-dashboard.component.css']
 })
 export class MemberDashboardComponent implements OnInit {
-
-  members$: Observable<Array<Member>>;
-
-  constructor(private store: Store<ApplicationState>) { }
+  constructor() {
+  }
 
   ngOnInit() {
-    this.store.dispatch(new LoadMembersAction());
-    this.members$ = this.store.pipe(select(MembersQuery.getMembers));
   }
 
 }
