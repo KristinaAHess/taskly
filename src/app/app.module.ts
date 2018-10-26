827eca8e0e40fd5cadf30fb01ba320fa92368381
KatjaNiklas <32980890+KatjaNiklas@users.noreply.github.com>
2018-10-26 12:45:29 +0200

    MemberModule and TaskModule added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {MemberModule} from './member/member.module';
+import {TaskModule} from './task/task.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MemberModule,
+    TaskModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
