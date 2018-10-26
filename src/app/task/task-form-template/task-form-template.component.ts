0f02e961f8905b4d930f0d43a6a72af233232200
KatjaNiklas <32980890+KatjaNiklas@users.noreply.github.com>
2018-10-26 12:58:19 +0200

    Task Components added

---
+++ src/app/task/task-form-template/task-form-template.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-task-form-template',
+  templateUrl: './task-form-template.component.html',
+  styleUrls: ['./task-form-template.component.css']
+})
+export class TaskFormTemplateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
