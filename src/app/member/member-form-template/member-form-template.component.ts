4265f35a8c40b227dfe3f932c37621850114f57b
Kristina Heß <kristina.hess@lht.dlh.de>
2018-10-26 11:57:23 +0100

    add Components for member module

---
+++ src/app/member/member-form-template/member-form-template.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-member-form-template',
+  templateUrl: './member-form-template.component.html',
+  styleUrls: ['./member-form-template.component.css']
+})
+export class MemberFormTemplateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
