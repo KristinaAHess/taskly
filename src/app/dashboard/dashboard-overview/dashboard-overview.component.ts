a664477ca648b9d00afe5fc5183d90fca84e7fa4
Kristina Heß <kristina.hess@lht.dlh.de>
2018-10-27 10:53:39 +0100

    get calender events from store, all events or for selected member

--- src/app/dashboard/dashboard-overview/dashboard-overview.component.ts
+++ src/app/dashboard/dashboard-overview/dashboard-overview.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ApplicationState } from 'src/app/state/app.state';
-import { Store } from '@ngrx/store';
-import { LoadMembersAction } from 'src/app/state/member/member.actions';
+import { select, Store } from '@ngrx/store';
+import { TasksQuery } from '../../state/task/task.reducer';
+import { Observable } from 'rxjs';
+import { CalendarEvent } from 'angular-calendar';
 
 @Component({
   selector: 'app-dashboard-overview',
@@ -9,10 +11,13 @@ import { LoadMembersAction } from 'src/app/state/member/member.actions';
   styleUrls: ['./dashboard-overview.component.css']
 })
 export class DashboardOverviewComponent implements OnInit {
+  events$: Observable<Array<CalendarEvent>>;
 
-  constructor(private store: Store<ApplicationState>) { }
+  constructor(private store: Store<ApplicationState>) {
+  }
 
   ngOnInit() {
+    this.events$ = this.store.pipe(select(TasksQuery.getEvents));
   }
 
 }
--- src/app/member/member-details/member-details.component.ts
+++ src/app/member/member-details/member-details.component.ts
@@ -6,6 +6,8 @@ import { Store, select } from '@ngrx/store';
 import { MembersQuery } from 'src/app/state/member/member.reducer';
 import { Member } from '../models/member';
 import { Observable } from 'rxjs';
+import { TasksQuery } from '../../state/task/task.reducer';
+import { CalendarEvent } from 'angular-calendar';
 
 @Component({
   selector: 'app-member-details',
@@ -13,16 +15,18 @@ import { Observable } from 'rxjs';
   styleUrls: ['./member-details.component.css']
 })
 export class MemberDetailsComponent implements OnInit {
-
+  events$: Observable<Array<CalendarEvent>>;
   member$: Observable<Member>;
 
-  constructor(private route: ActivatedRoute, private store: Store<ApplicationState>, private router: Router) { }
+  constructor(private route: ActivatedRoute, private store: Store<ApplicationState>, private router: Router) {
+  }
 
   ngOnInit() {
     const id = this.route.snapshot.paramMap.get('id');
     this.store.dispatch(new LoadMembersAction());
     this.store.dispatch(new SelectMemberAction(+id));
     this.member$ = this.store.pipe(select(MembersQuery.getSelectedMember));
+    this.events$ = this.store.pipe(select(TasksQuery.getEventsforSelectedMember));
   }
 
   navigateToEditor() {
